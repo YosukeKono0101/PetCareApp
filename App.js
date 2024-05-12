e7f1150ca4c205599d00e741f4bc81661329854c
Yosuke Kono <n11230380@qut.edu.au>
2024-05-12 17:53:01 +1000

    [FIX] added an dependency with the SDK update

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./src/navigation/AppNavigator";
+import "react-native-reanimated";
 
 export default function App() {
   return (
